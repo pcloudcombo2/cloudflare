5a1943dbb29868c97513c9107322d9d168cd58df
pcloudcombo2 <122129928+pcloudcombo2@users.noreply.github.com>
2026-01-01 05:01:58 -0600

    Add User-Agent header to API request

--- worker.js
+++ worker.js
@@ -44,6 +44,8 @@ export default {
         {
           headers: {
             Authorization: `Bearer ${tokenData.token}`,
+            "User-Agent":
+              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
           },
         }
       );
